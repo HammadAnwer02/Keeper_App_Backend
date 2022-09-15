c897fd12980dace9ac7cd36a24b7c7c7ffee62ab
Hammad <hammadanwer76@gmail.com>
2022-09-15 15:27:22 +0500

    added cors

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ const cors = require('cors');
 
 const corsOptions = {
   origin : 'http://localhost:3000',
+  credentials: true,
   optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
+
 app.use(require("./routes/notes"));
 //connect to the database
 dbConnect();
