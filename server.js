abfcd66ae01fd3e3415d6f11d30a128c47053d56
Hammad <hammadanwer76@gmail.com>
2022-09-15 15:19:52 +0500

    Updated cors

--- server.js
+++ server.js
@@ -13,17 +13,17 @@ dbConnect();
 
 // Curb Cores Error by adding a header here
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
   
 // configure use of bodyParser 
 app.use(bodyParser.json());
